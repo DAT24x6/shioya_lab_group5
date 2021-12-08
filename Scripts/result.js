ed7ce34f8f584b8e6180e0a07d2c677825fe2fb8
DAT24x6 <daisuke19300@gmail.com>
2021-12-09 02:10:52 +0900

    update: コード修正
    ファイル構成やファイル名の変更とそれに影響するコードの修正、タイトル等の統一、インデント修正。

---
+++ Scripts/result.js
@@ -0,0 +1,18 @@
+const currentUrl = new URL(location.href);
+//URLSearchParamsオブジェクトを取得
+const queryString = currentUrl.searchParams;
+
+
+//取得方法1 : キー名を指定して値を取得
+const namae = queryString.get('yourName');
+document.write(namae);
+const job = queryString.get('yourJob');
+document.write(job);
+const gakubu = queryString.get('yourSyozoku');
+document.write(gakubu);
+const year = queryString.get('year');
+document.write(year);
+const month = queryString.get('month');
+document.write(month);
+const day = queryString.get('day');
+document.write(day);
