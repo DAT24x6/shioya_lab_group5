cba4d5cae7f6f05c4d966e0b300a68c577251265
shoumaa1216 <shoumaa1216@gmail.com>
2021-12-27 23:00:01 +0900

    html,jsコード追加,css新規ファイル作成

--- Scripts/result.js
+++ Scripts/result.js
@@ -5,14 +5,18 @@ const queryString = currentUrl.searchParams;
 
 //取得方法1 : キー名を指定して値を取得
 const namae = queryString.get('yourName');
-document.write(namae);
+document.write("<p class= 'namae'>" + namae + "</p>");
+const shasin = queryString.get('yourPicture');
+document.write("<img src=\"" + shasin + "\">");
+const email = queryString.get('email');
+document.write("<p class= 'email'>" + email + "</p>");
 const job = queryString.get('yourJob');
-document.write(job);
+document.write("<p class= 'job'>" + job + "</p>");
 const gakubu = queryString.get('yourSyozoku');
-document.write(gakubu);
+document.write("<p class= 'gakubu'>" + gakubu + "</p>");
 const year = queryString.get('year');
-document.write(year);
+document.write("<p class= 'year'>" + year + "</p>");
 const month = queryString.get('month');
-document.write(month);
+document.write("<p class= 'month'>" + month + "</p>");
 const day = queryString.get('day');
-document.write(day);
+document.write("<p class= 'day'>" + day + "</p>");
