5747e073519deeff13d20385730359cae71c4c32
shoumaa1216 <shoumaa1216@gmail.com>
2022-01-10 04:58:04 +0900

    ローカルストレージに変更

--- Scripts/database.js
+++ Scripts/database.js
@@ -1,15 +1,15 @@
-const key1 =sessionStorage.getItem('key1');
+const key1 =localStorage.getItem('key1');
 
-const key2 =sessionStorage.getItem('key2');
+const key2 =localStorage.getItem('key2');
 
-const key3 =sessionStorage.getItem('key3');
+const key3 =localStorage.getItem('key3');
 
-const key4 =sessionStorage.getItem('key4');
+const key4 =localStorage.getItem('key4');
 
-const key5 =sessionStorage.getItem('key5');
+const key5 =localStorage.getItem('key5');
 
-const key6 =sessionStorage.getItem('key6');
+const key6 =localStorage.getItem('key6');
 
-const key7 =sessionStorage.getItem('key7');
+const key7 =localStorage.getItem('key7');
 
-const key8 =sessionStorage.getItem('key8');
+const key8 =localStorage.getItem('key8');
