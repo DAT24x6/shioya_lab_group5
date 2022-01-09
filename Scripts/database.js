883629fab3f0855a8a679751cfde2a224d493744
shoumaa1216 <shoumaa1216@gmail.com>
2022-01-10 03:42:56 +0900

    database.htmlにcss適用

---
+++ Scripts/database.js
@@ -0,0 +1,15 @@
+const key1 =sessionStorage.getItem('key1');
+
+const key2 =sessionStorage.getItem('key2');
+
+const key3 =sessionStorage.getItem('key3');
+
+const key4 =sessionStorage.getItem('key4');
+
+const key5 =sessionStorage.getItem('key5');
+
+const key6 =sessionStorage.getItem('key6');
+
+const key7 =sessionStorage.getItem('key7');
+
+const key8 =sessionStorage.getItem('key8');
