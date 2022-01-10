35024a2c9f2f7b1496f80de15f4640f16570cedc
shoumaa1216 <shoumaa1216@gmail.com>
2022-01-10 11:58:03 +0900

    フォーム画面の写真項目削除

--- Scripts/result.js
+++ Scripts/result.js
@@ -6,8 +6,6 @@ const queryString = currentUrl.searchParams;
 //取得方法1 : キー名を指定して値を取得
 const namae = queryString.get('yourName');
 
-const shasin = queryString.get('yourPicture');
-document.write("<img src=\"" + shasin + "\">");
 const email = queryString.get('email');
 
 const gender = queryString.get('gender');
